38fd019eed194a982d18c3e37b505fa9ad916e91
stevec-afk <evets25@gmail.com>
2025-10-20 11:10:28 -0300

    06_repeatStrings solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0){
+        return "ERROR"
+    }
+    let repeat = "";
+    for (let i = 0; i < times; i++){
+        repeat += word;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
