bd0766ae5124db1936d0f3dfaeae92a88d257938
stevec-afk <evets25@gmail.com>
2025-10-20 11:22:33 -0300

    07_reverseString solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversed = "";
+    for (let i=input.length;i>0;i--){
+        reversed += input.charAt(i-1);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
