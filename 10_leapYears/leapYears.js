d0490b60fe755ca0524457b8b64e03f62741c805
stevec-afk <evets25@gmail.com>
2025-10-21 10:54:02 -0300

    10_leapYear solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4 === 0){
+        if (year%100 == 0 && year%400 != 0){
+            return false;
+        }
+        else return true;
+    }
+    else return false;
 };
+
+// Condensed 1-line version:
+// return (year%4 ===0 && (year%100 !== 0 || year%400 == 0))
+
 
 // Do not edit below this line
 module.exports = leapYears;
