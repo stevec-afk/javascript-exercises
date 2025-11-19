7d488c6e0dd2d213b2f82521d73ba94521581970
stevec-afk <evets25@gmail.com>
2025-11-19 11:27:59 -0400

    13_palindromes solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    parsedString=string.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
 
+    let reversedString = parsedString
+        .split('')
+        .reverse()
+        .join('');
+    return reversedString === parsedString;
 };
 
 // Do not edit below this line
