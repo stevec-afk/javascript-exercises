cb2eeb63fa69395d67f314c00093d7b451f75d5a
stevec-afk <evets25@gmail.com>
2025-11-19 10:59:39 -0400

    12_calculator solution

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x , y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  // we also should be checking for negative numbers, but there is no test for that.
+  if (x < 2) {
+    return 1; 
+  }
+
+	let total = 1;
+  for (i=2;i<=x;i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
