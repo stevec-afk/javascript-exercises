12d598818f3f37fb988b971de10835d46b09cec0
stevec-afk <evets25@gmail.com>
2025-11-19 12:38:23 -0400

    14_fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    
+    let fib = [0,1];
+    for (let i=2;i<=x;i++){
+        fib.push(fib[i-1]+fib[i-2]);
+    }
+    return fib[x];
 };
 
 // Do not edit below this line
