5802bb93dbcc9f77c0e54ffd943d5c0ffc489d46
stevec-afk <evets25@gmail.com>
2026-05-14 13:35:49 -0300

    resolve merge conflicts from upstream

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
+function factorial (x) {
+    if (x === 0) return 1;
 
+    let total = 0;
+    total+=factorial(total-1);
+    return total;
 };
 
 // Do not edit below this line
