d2e56b40ab545f96caf1d1a0f7d1045ef05ba3d1
stevec-afk <evets25@gmail.com>
2026-05-15 12:50:17 -0300

    03_totalIntegers solution

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,25 @@
-const totalIntegers = function() {
-  
+// Same as before, I'm using iteration rather than recursion to solve
+
+const totalIntegers = (obj) => {
+    if (typeof obj !== 'object' || obj === null) return undefined;
+
+    const stack = [obj];
+    let count = 0;
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+    
+        if (Number.isInteger(current)) {
+            count++;
+            continue;
+        }       
+        if (typeof current === "object" && current !== null){
+            for (const value of Object.values(current)) {
+                stack.push(value);
+            }
+        }
+    }
+    return count;
 };
   
 // Do not edit below this line
